cabfc978e949888526e6dd70762de89fac609a47
KredikShaw <alekskn999@gmail.com>
2019-12-15 00:03:06 +0200

    more js

---
+++ numbers.js
@@ -0,0 +1,68 @@
+// $(allInView);
+// $(window).scroll(allInView);
+
+function animateValue(id, start, end, duration) {
+    // assumes integer values for start and end
+    
+    var obj = document.getElementById(id);
+    var range = end - start;
+    // no timer shorter than 50ms (not really visible any way)
+    var minTimer = 50;
+    // calc step time to show all interediate values
+    var stepTime = Math.abs(Math.floor(duration / range));
+    
+    // never go below minTimer
+    stepTime = Math.max(stepTime, minTimer);
+    
+    // get current time and calculate desired end time
+    var startTime = new Date().getTime();
+    var endTime = startTime + duration;
+    var timer;
+  
+    function run() {
+        var now = new Date().getTime();
+        var remaining = Math.max((endTime - now) / duration, 0);
+        var value = Math.round(end - (remaining * range));
+        obj.innerHTML = value;
+        if (value == end) {
+            clearInterval(timer);
+        }
+    }
+    
+    timer = setInterval(run, stepTime);
+    run();
+}
+
+
+// function isScrolledIntoView(elem) {
+//     var docViewTop = $(window).scrollTop();
+//     var docViewBottom = docViewTop + $(window).height();
+
+//     var elemTop = $(elem).offset().top;
+//     var elemBottom = elemTop + $(elem).height();
+
+//     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+// }
+
+// function allInView() {
+
+//     if (isScrolledIntoView($(".numbers-description"))){
+        
+        
+//         animateValue("value1", 0, 150, 2000);
+//         animateValue("value2", 0, 100, 2000);
+//         animateValue("value3", 0, 3, 2000);
+//     }
+// }
+
+var observer = new IntersectionObserver(function(entries) {
+	// isIntersecting is true when element and viewport are overlapping
+	// isIntersecting is false when element and viewport don't overlap
+	if(entries[0].isIntersecting === true)
+        console.log('Element has just become visible in screen');
+        animateValue("value1", 0, 150, 1600);
+        animateValue("value2", 0, 100, 1600);
+        kanimateValue("value3", 0, 3, 1600);
+}, { threshold: [0] });
+
+observer.observe(document.querySelector("#aaa"));
